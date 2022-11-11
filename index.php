8c046d1e165d5097abe9cb8d7eb5ca4232e5012d
Ala'a Jber Karaja <33665712+AlaaJkaraja@users.noreply.github.com>
2022-11-11 20:47:42 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="post" action="" enctype="multipart/form-data">
+        <input type="file" name="file[]" value="" multiple>
+        <br><br><br>
+        <input type="submit" name="submit" value="Upload">
+    </form>
+</body>
+
+</html>
+<?php
+$isUploaded = false;
+if (isset($_POST["submit"])) {
+    $filesCount = count($_FILES["file"]["name"]);
+    for ($i = 0; $i < $filesCount; $i++) {
+        $file_name = $_FILES["file"]["name"][$i];
+        $file_tem = $_FILES["file"]["tmp_name"][$i];
+        $file_size = $_FILES["file"]['size'][$i];
+        $file_type = $_FILES["file"]['type'][$i];
+
+        $allowed_extensions = array('pdf', 'docx', 'png', 'gpeg');
+        $file_name_splitted = explode('.', $file_name);
+        $file_ex = end($file_name_splitted);
+
+        if (!in_array($file_ex, $blocked_extensions)) {
+            $file_ex = strtolower($file_ex);
+            $newFileName = rand(100000, 900000) . '.' . $file_ex;
+            $target = "folderToUpload/" . $newFileName;
+            $upload = move_uploaded_file($file_tem, $target);
+            $isUploaded = true;
+        } else {
+            die("Blocked File Type");
+        }
+    }
+    if ($isUploaded) {
+        die("Upload successfully");
+    } else {
+        die("Upload unsuccessfully");
+    }
+} else {
+    die("There is no file to upload.");
+}
